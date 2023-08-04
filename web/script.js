960d9622416dc3ed6256f262680313fa5b95ac64
Kruthi16 <108337356+Kruthi16@users.noreply.github.com>
2023-08-04 23:21:56 +0530

    Delete web directory

--- web/script.js
+++
@@ -1,8 +0,0 @@
-document.querySelector("button").onclick = function () {
-    
-    eel.predict()(function(crop){                   
-        
-        document.querySelector(".a").innerHTML = crop;
-    })
-    }
-    
